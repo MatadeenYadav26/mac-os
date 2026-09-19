717ec07ad7dd629bd215165ad1a79100a69d6565
MatadeenYadav <scrollwithamanoficial@outlook.com>
2026-09-20 02:09:02 +0530

    Day 1: Setup project and create initial dock

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './app.scss'
+import { useState } from 'react'
+import Dock from './components/Dock.jsx'
+
+
+function App()  {
+  return (
+    <main>
+      <Dock />
+    </main>
+  )
+}
+
+export default App
