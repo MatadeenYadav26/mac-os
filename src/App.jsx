f33680011294253752650f3aa8b27811e931c40f
MatadeenYadav <scrollwithamanoficial@outlook.com>
2026-09-22 21:02:01 +0530

    Day 3: Add resizable macOS window using React Rnd

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import './app.scss'
 import Dock from './components/Dock.jsx'
 import Nav from './components/Nav.jsx'
+import MacWindow from './components/windows/MacWindow.jsx'
 
 function App() {
   return (
     <main>
       <Nav />
       <Dock />
+
+        <MacWindow>
+        <h1>Hello User!</h1>
+      </MacWindow>
     </main>
   )
 }
