4edd3b3df8e09be94edd9da06c4352119c2ecfb2
MatadeenYadav <scrollwithamanoficial@outlook.com>
2026-09-25 00:06:10 +0530

    Day 4: Add GitHub portfolio and improve navbar layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './app.scss'
 import Dock from './components/Dock.jsx'
 import Nav from './components/Nav.jsx'
 import MacWindow from './components/windows/MacWindow.jsx'
+import Github from './components/windows/Github.jsx'
 
 function App() {
   return (
@@ -10,9 +11,8 @@ function App() {
       <Nav />
       <Dock />
 
-        <MacWindow>
-        <h1>Hello User!</h1>
-      </MacWindow>
+      <Github />
+    
     </main>
   )
 }
